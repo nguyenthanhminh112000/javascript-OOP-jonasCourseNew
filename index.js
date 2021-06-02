a78de78da59a51fdff298d5981eb63012eee973b
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-02 14:56:59 +0700

    PROTOTYPAL INHERITANCE ON BUILD-IN OBJECTS LIKE ARRAY, FUNCTION

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-/////////////////////////////////////// FUNCTION CONSTRUCTOR, PROTOTYPE, PROTOTYPE CHAIN, PROTOTYPAL INHERITANCE
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.logInfo = function (params) {
-    console.log(`from constructor`);
-  };
-}
-Person.prototype.fullName = 'Nguyen';
-Person.prototype.logInfo = function () {
-  console.log(`${this.name} ${this.fullName} ${this.age}`);
+// /////////////////////////////////////// FUNCTION CONSTRUCTOR, PROTOTYPE, PROTOTYPE CHAIN, PROTOTYPAL INHERITANCE
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.logInfo = function (params) {
+//     console.log(`from constructor`);
+//   };
+// }
+// Person.prototype.fullName = 'Nguyen';
+// Person.prototype.logInfo = function () {
+//   console.log(`${this.name} ${this.fullName} ${this.age}`);
+// };
+
+// const minh = new Person('minh', 20);
+// console.log(minh);
+// console.log(minh.__proto__ === Person.prototype);
+// console.log(Person.prototype.constructor === Person);
+// console.log(minh.__proto__.__proto__.__proto__);
+
+//////////////////////////////////////////// PROTOTYPAL INHERITANCE ON BUILD-IN OBJECTS
+const arr = [1, 2, 3];
+console.log(arr);
+console.log(arr.hasOwnProperty('length'));
+console.log(arr.hasOwnProperty('2'));
+console.log(arr.__proto__.__proto__.__proto__);
+
+const func = () => {
+  console.log('function');
 };
 
-const minh = new Person('minh', 20);
-console.log(minh);
-console.log(minh.__proto__ === Person.prototype);
-console.log(Person.prototype.constructor === Person);
-console.log(minh.__proto__.__proto__.__proto__);
+console.dir(func);
+console.log(func.hasOwnProperty('arguments'));
+console.log(func.hasOwnProperty('caller'));
+console.log(func.hasOwnProperty('name'));
+console.log(func.hasOwnProperty('length'));

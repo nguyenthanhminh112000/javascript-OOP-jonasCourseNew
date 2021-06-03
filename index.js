0fa8c99f72d95e2d74ace1fbe7fa302fd1f6f99d
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-03 17:21:20 +0700

    STATIC METHOD AND PROPERTY

--- index.js
+++ index.js
@@ -49,67 +49,99 @@
 // console.log(johnDNguyen);
 // console.log(johnDNguyen.__proto__ === PersonCl.prototype);
 
-////////////////////////////////////////// GETTERS AND SETTERS
-// const john = {
-//   name: 'John',
-//   lastName: 'Nguyen',
-//   properties: ['house-284 Tran Phu'],
+// ////////////////////////////////////////// GETTERS AND SETTERS
+// // const john = {
+// //   name: 'John',
+// //   lastName: 'Nguyen',
+// //   properties: ['house-284 Tran Phu'],
 
-//   get getProp() {
-//     return this.properties;
-//   },
+// //   get getProp() {
+// //     return this.properties;
+// //   },
 
-//   set setProp(property) {
-//     this.properties.push(property);
-//   },
+// //   set setProp(property) {
+// //     this.properties.push(property);
+// //   },
 
-//   getProps: function () {
-//     return this.properties;
-//   },
-//   setProps: function (property) {
-//     this.properties.push(property);
-//   },
-// };
-// console.log(john);
-// console.log(john.getProp);
-// john.setProp = 'land-Ngo Quyen';
-// console.log(john);
-// console.log(john.getProps());
-// john.setProps('land-Hung Loc');
-// console.log(john);
-// console.log(john.hasOwnProperty('setProp'));
-// console.log(john.hasOwnProperty('setProp'));
+// //   getProps: function () {
+// //     return this.properties;
+// //   },
+// //   setProps: function (property) {
+// //     this.properties.push(property);
+// //   },
+// // };
+// // console.log(john);
+// // console.log(john.getProp);
+// // john.setProp = 'land-Ngo Quyen';
+// // console.log(john);
+// // console.log(john.getProps());
+// // john.setProps('land-Hung Loc');
+// // console.log(john);
+// // console.log(john.hasOwnProperty('setProp'));
+// // console.log(john.hasOwnProperty('setProp'));
+// class PersonCl {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   calAgeVersion2() {
+//     console.log(this.age);
+//   }
+
+//   // get age() {
+//   //   return this._age;
+//   // }
+
+//   set age(age) {
+//     this._age = age;
+//   }
+
+//   // get name() {
+//   //   return this._name;
+//   // }
+
+//   set name(name) {
+//     if (name.includes(' ')) {
+//       this._name = name;
+//     } else {
+//       console.log(`${name} isn't a fullname`);
+//     }
+//   }
+// }
+
+// const jessica = new PersonCl('Jessica', 20);
+// console.log(jessica);
+// console.log(jessica.name);
+// console.log(jessica.age);
+
+////////////////////////////////////////// STATIC METHOD & PROPERTY
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.greet = function () {
+  console.log('Greet from Person static method');
+  console.log(this);
+};
+const john = new Person('john d nguyen', 20);
+Person.greet();
+Person.namee = 'minh';
+console.log(Person.namee);
+console.log(john);
+
 class PersonCl {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+  static greet() {
+    console.log(`Hello everyone`);
   }
-  calAgeVersion2() {
-    console.log(this.age);
-  }
-
-  // get age() {
-  //   return this._age;
-  // }
-
-  set age(age) {
-    this._age = age;
-  }
-
-  // get name() {
-  //   return this._name;
-  // }
-
-  set name(name) {
-    if (name.includes(' ')) {
-      this._name = name;
-    } else {
-      console.log(`${name} isn't a fullname`);
-    }
-  }
+  static namee = 'minh';
+  static greetVersion2 = function () {
+    console.log('Hello everyone version 2');
+  };
 }
 
-const jessica = new PersonCl('Jessica', 20);
-console.log(jessica);
-console.log(jessica.name);
-console.log(jessica.age);
+const person1 = new PersonCl();
+console.log(person1);
+PersonCl.greet();
+PersonCl.greetVersion2();
+console.log(PersonCl.namee);
